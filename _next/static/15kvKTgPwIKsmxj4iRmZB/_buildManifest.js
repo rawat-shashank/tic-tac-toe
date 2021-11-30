06e17109e8736209d492a375156700ad8600c614
rawat-shashank <shashank17nov@gmail.com>
2021-11-30 11:30:14 +0000

    Deploying to gh-pages from @ rawat-shashank/tic-tac-toe@1c106465cf94ceef3091924a16fdafde45a3d18b 🚀

---
+++ _next/static/15kvKTgPwIKsmxj4iRmZB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/26af6298f33502b4.css","static/chunks/pages/index-9899017516a4c1b1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
